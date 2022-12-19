73f09f1ca624c8eb8d8af4206e20200b9bdec356
Samuel Alejandro Maldonado Garcia <samm3488@gmail.com>
2022-12-19 14:06:25 -0400

    First commit

---
+++ src/app/contador/contador/contador.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-contador',
+    template: `
+<h1>{{title}}</h1>
+<h3>The base is :<strong>{{base}}</strong></h3>
+<button (click)="calc(base)">+ {{base}}</button>
+<span>{{number}}</span>
+<button (click)="calc(-base)">- {{base}}</button>
+`
+})
+
+export class ContadorComponent {
+    title: string = 'Contador APP';
+    number: number = 10;
+    base: number = 5;
+
+    calc(valor: number) {
+        this.number += valor;
+    }
+
+}
---
+++ src/app/heroes/listado/listado.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado',
+  templateUrl: './listado.component.html',
+  styleUrls: ['./listado.component.css']
+})
+export class ListadoComponent {
+ heroes:string[]=['spiderman','ironman','hulk','thor']
+ deletedHero:string='';
+ deletedHeroes(){
+   this.deletedHero= this.heroes.shift()||'';
+ }
+}
